df7dd20b3a6c7cc02151a7e7ae3a6f6e48f8fde1
Domingos Canhanga <canhanga96@gmail.com>
2025-08-03 00:33:25 +0100

    refactor: update logo make logo dynamic between dark or light

--- src/components/landingPage/navbar/logo.tsx
+++ src/components/landingPage/navbar/logo.tsx
@@ -1,13 +1,17 @@
 'use client'
+import { useTheme } from 'next-themes'
 import Image from 'next/image'
 
 export function Logo() {
+  const { theme } = useTheme()
+
   return (
     <Image
+      suppressHydrationWarning
       alt="logo"
       className="cursor-pointer"
       height={50}
-      src="/logo.png"
+      src={theme === 'dark' ? '/white-logo.png' : '/logo.png'}
       width={146}
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
     />
